d6c2d61110487bfb31a5a24191b4016eca79a96f
siuming <siumingdev@gmail.com>
2020-11-01 21:55:10 +0800

    update algo string week3

--- Data Structures and Algorithms, UCSD/4. Algorithms on Strings/week3/kmp (done)/kmp.py
+++ Data Structures and Algorithms, UCSD/4. Algorithms on Strings/week3/kmp (done)/kmp.py
@@ -2,6 +2,20 @@
 import sys
 
 
+def compute_prefix_function(p):
+  s = [0]
+  border = 0
+  for i in range(1, len(p)):
+    while (border > 0) and (p[i] != p[border]):
+      border = s[border - 1]
+    if p[i] == p[border]:
+      border = border + 1
+    else:
+      border = 0
+    s.append(border)
+  return s
+
+
 def find_pattern(pattern, text):
   """
   Find all the occurrences of the pattern in the text
@@ -10,6 +24,11 @@ def find_pattern(pattern, text):
   """
   result = []
   # Implement this function yourself
+  S = pattern + '$' + text
+  s = compute_prefix_function(S)
+  for i in range(len(pattern) + 1, len(S)):
+    if s[i] == len(pattern):
+      result.append(i - 2 * len(pattern))
   return result
 
 
